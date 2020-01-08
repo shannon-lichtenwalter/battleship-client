c85744929cc7580eda93cb1bcc9369744ef4923b
Heesu Kang <hskang135@gmail.com>
2020-01-08 12:11:11 -0500

    update login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import AuthApiService from '../../Services/auth-api-service';
+import UserContext from '../../Contexts/UserContext';
+import { Input, Required, Label } from '../Form/Form';
+import Button from '../Button/Button';
 import './Login.css';
 
 export default class Login extends Component {
+  static defaultProps = {
+    onLoginSuccess: () => { }
+  }
+
+  static contextType = UserContext
+
+  state = { error: null }
+
+  firstInput = React.createRef()
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    const { username, password } = ev.target
+
+    this.setState({ error: null })
+
+    AuthApiService.postLogin({
+      username: username.value,
+      password: password.value,
+    })
+      .then(res => {
+        username.value = ''
+        password.value = ''
+        this.context.processLogin(res.authToken)
+        this.props.onLoginSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
+  }
+
+  componentDidMount() {
+    this.firstInput.current.focus()
+  }
 
   render() {
     return (
       <form className='loginform'>
         <h1>Login</h1>
         <div>
-          <label htmlFor='login-username-input'>Username</label>
-          <input
+          <Label htmlFor='login-username-input'>Username<Required /></Label>
+          <Input
+            ref={this.firstInput}
             id='login-username-input'
             name='username'
             required
@@ -18,20 +57,21 @@ export default class Login extends Component {
         </div>
 
         <div>
-          <label htmlFor='login-password-input'>Password</label>
-          <input
+          <Label htmlFor='login-password-input'>Password<Required /></Label>
+          <Input
             id='login-password-input'
             name='password'
             required
           />
         </div>
 
-        <button type='submit'>Login</button>
-        <button>
+        <Button type='submit'>Login</Button>
+        {' '}
+        <Button>
           <Link to='/guest'>
             Guest
           </Link>
-        </button> <br />
+        </Button> <br />
 
         <Link to='/signup'>Need to create an account?</Link>
       </form>
