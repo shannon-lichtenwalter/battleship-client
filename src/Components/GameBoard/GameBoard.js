a8ba5a04f2925bc574ca99015eb1d0fdf70771ba
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-01-08 19:22:24 -0700

    api calls working for setting up ships and for firing at target

--- src/Components/GameBoard/GameBoard.js
+++ src/Components/GameBoard/GameBoard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserGrid from '../UserGrid/UserGrid';
 import OpponentGrid from '../OpponentGrid/OpponentGrid';
+import BattleShipContext from '../../Contexts/battleship-context';
 import './GameBoard.css';
 
 class GameBoard extends React.Component {
@@ -34,18 +35,37 @@ class GameBoard extends React.Component {
     userMisses: [],
     opponentHits: [],
     opponentMisses: [],
-    userTurn: false
+    userTurn: false,
+    //hard coding gameId and playerNum for testing purposes temporarily
+    gameId: 9,
+    playerNum: 'player1',
+    error: null,
+  }
+
+  //can we move this to a separate context provider file?
+  setError = (err) => {
+    this.setState({
+      error: err.error
+    })
   }
 
   render () {
     return (
+      <BattleShipContext.Provider value={{
+        gameId: this.state.gameId,
+        playerNum: this.state.playerNum,
+        error: this.state.error,
+        setError: this.setError
+      }}>
       <>
+      {this.state.error && <p className='errorMessage'>{this.state.error}</p>}
       <h2>Your Ships</h2>
       <UserGrid />
 
       <h2>Opponent Ships</h2>
       <OpponentGrid />
       </>
+      </BattleShipContext.Provider>
     )
   }
 }
--- src/Components/UserGrid/UserGrid.js
+++ src/Components/UserGrid/UserGrid.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Cell from '../Cell/Cell';
-import './UserGrid.css'
+import './UserGrid.css';
+import gameMovesApiService from '../../game-moves-api-service';
+import BattleShipContext from '../../Contexts/battleship-context';
 
 class UserGrid extends React.Component {
 
@@ -19,6 +21,8 @@ class UserGrid extends React.Component {
         }
     }
 
+    static contextType = BattleShipContext;
+
     componentDidMount() {
 
     }
@@ -26,10 +30,14 @@ class UserGrid extends React.Component {
     //This function is called by the render. It will look at the counter value to determine
     // if the user still needs to set their ship locations or if all the ship values have been set.
     //counter was added to state in order to access the different ships, counter is incremeneted in a later function
-    //after a boat has been completely built.
+    //after a boat has been completely built. If all the boats have been build, then an API call is made to update the
+    //player's ships location in the database.
 
     handleSetShips = () => {
         if(this.state.counter > 4){
+            //once all the ships are set, the data is sent to the database to be stored
+            gameMovesApiService.setShips(this.state.playerShips, this.context.gameId, this.context.playerNum)
+            .catch((e) => this.context.setError(e));
             return `All Ships Have Been Set`
         } else {
             return `Please select cells for ${this.state.playerShips[this.state.counter].name}.
@@ -42,16 +50,19 @@ class UserGrid extends React.Component {
     //for the respective boats. We may not need this later on, but is helpful to see which cells are representing the boat so far.
     displayBoats = () => {
         return this.state.playerShips.map((ship, index) => {
-            return <li key={index}>{ship.name} : {ship.spaces.length !== 0 ? ship.spaces.map(space => space.value + ', ') : 'ship not built yet'}</li>
+            return <li key={index}>{ship.name} : {ship.spaces.length !== 0 ? ship.spaces.join(', ') : 'ship not built yet'}</li>
         })
     }
 
     //this function is used as a callback function after updating the boat values in state. this will allow us to check and see if the
-    //boat is finishe being built. if so it will update the playerShips in state with the values.
+    //boat is finished being built. if so it will update the playerShips in state with the values. It will also reset the boat back to empty and will 
+    //increment the counter in order to move on to the next boat to build.
     handleCheckBoatLength = () => {
         if(this.state.boat.length === this.state.playerShips[this.state.counter].length){
             let currentShips = this.state.playerShips;
-            currentShips[this.state.counter].spaces = this.state.boat
+            let boatValues = this.state.boat.map(boat => boat.value);
+            console.log(boatValues);
+            currentShips[this.state.counter].spaces = boatValues
             this.setState({
                 playerShips: currentShips,
                 counter: this.state.counter + 1,
@@ -59,6 +70,8 @@ class UserGrid extends React.Component {
             })
         } 
     }
+
+    //this will be replaced by Sean's logic that he is building.
 
     handleCheckValue = (value, idNum) => {
         if(this.state.boat.length === 0){
