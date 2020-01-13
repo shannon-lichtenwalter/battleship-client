81f87611064b2938e81f4307db2172214eb40c4f
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-01-13 12:27:02 -0700

    resume game working

--- src/Components/Cell/Cell.js
+++ src/Components/Cell/Cell.js
@@ -3,144 +3,156 @@ import './Cell.css';
 
 class Cell extends React.Component {
 
-  static defaultProps ={
-    selected:null,
-    idNumber:-2,
-    shipTiles:[],
-    allShipTiles:[],
+  static defaultProps = {
+    selected: null,
+    idNumber: -2,
+    shipTiles: [],
+    allShipTiles: [],
   }
 
-constructor(props){
-  super(props);
+  constructor(props) {
+    super(props);
     this.state = {
       x: this.props.x,
       y: this.props.y,
       occupied: null,
       hasBeenShot: null,
-      index: this.findMyIndex, 
-      isShipTile:false, 
-  }
-  //this.findMyIndex = this.findMyIndex.bind(this)
-}
-
-// componentDidMount(){
-//   if ((this.props.shipTiles) && (this.props.shipTiles.indexOf(this.props.currentId)!==(-1))){
-//     this.setState({shipTile:true})
-//   }
-//   this.checkForShipTile()
-// }
-
-// updateShipTile=(value, selected)=>{
-//   console.log(value)
-//   console.log(selected)
-//   //console.log(this.state.x + this.state.y)
-//   if(value === selected){
-//     this.setState({shipTile : true})
-//   }
-// }
-
-determineClassName = () => {
-  //console.log(this.props.selected)
-  //console.log(this.props.id)
-  let className = 'cell';
-  if(this.props.hits && this.props.hits.includes(this.props.id)){
-    className += ' hit'
-  } else
-  if (this.props.misses && this.props.misses.includes(this.props.id)){
-    className += ' miss'
+      index: this.findMyIndex,
+      isShipTile: false,
+    }
+    //this.findMyIndex = this.findMyIndex.bind(this)
   }
 
-  // the following is determining classNames for cells of resumedGames only.
-  if(this.props.resumedGame){
-    let shipSpaces = [];
-    this.props.playerShips.map(ship => {
-      return ship.spaces.map(space => shipSpaces.push(space))
-    })
-      if(shipSpaces.includes(this.props.id)){
+  // componentDidMount(){
+  //   if ((this.props.shipTiles) && (this.props.shipTiles.indexOf(this.props.currentId)!==(-1))){
+  //     this.setState({shipTile:true})
+  //   }
+  //   this.checkForShipTile()
+  // }
+
+  // updateShipTile=(value, selected)=>{
+  //   console.log(value)
+  //   console.log(selected)
+  //   //console.log(this.state.x + this.state.y)
+  //   if(value === selected){
+  //     this.setState({shipTile : true})
+  //   }
+  // }
+
+  determineClassName = () => {
+    //console.log(this.props.selected)
+    //console.log(this.props.id)
+    let className = 'cell';
+    if (this.props.hits && this.props.hits.includes(this.props.id)) {
+      className += ' hit'
+    } else
+      if (this.props.misses && this.props.misses.includes(this.props.id)) {
+        className += ' miss'
+      }
+
+    // the following is determining classNames for cells of resumedGames only.
+    if (this.props.resumedGame) {
+      let shipSpaces = [];
+      this.props.playerShips.map(ship => {
+        return ship.spaces.map(space => shipSpaces.push(space))
+      })
+      if (shipSpaces.includes(this.props.id)) {
         className += ' ship'
-        if(this.props.opponentHits){
-          if(this.props.opponentHits.includes(this.props.id)){
-          className += '-shot'
+        if (this.props.opponentHits) {
+          if (this.props.opponentHits.includes(this.props.id)) {
+            className += '-shot'
           }
         }
-      }else if (this.props.opponentMisses){
-        if(this.props.opponentMisses.includes(this.props.id)) {
-        className += ' shot'
+        if (this.props.opponentShots) {
+          if (this.props.opponentShots.includes(this.props.id)) {
+            className += '-shot'
+          }
+        }
+      } else {
+        if (this.props.opponentMisses) {
+          if (this.props.opponentMisses.includes(this.props.id)) {
+            className += ' shot'
+          }
+        }
+        
+        if(this.props.opponentShots) {
+          if (this.props.opponentShots.includes(this.props.id)) {
+            className += ' shot'
+          }
         }
       }
-    };
-
-
-  if(this.props.allShipTiles.length > 0){
-      if (this.props.allShipTiles.indexOf(this.props.idNumber) !== (-1)){
+    }
+    //the following is the logic for classNames for new games
+    if (this.props.allShipTiles.length > 0) {
+      if (this.props.allShipTiles.indexOf(this.props.idNumber) !== (-1)) {
         className += ' ship'
-        if(this.props.opponentShots && this.props.opponentShots.includes(this.props.id)) {
+        if (this.props.opponentShots && this.props.opponentShots.includes(this.props.id)) {
           className += '-shot'
         }
-      } else if(this.props.opponentShots && this.props.opponentShots.includes(this.props.id)) {
+      } else if (this.props.opponentShots && this.props.opponentShots.includes(this.props.id)) {
         className += ' shot'
       }
-  
-  } 
 
-  else if (this.props.selected === this.props.id) {
-    className += ' selected'
-    //console.log(this.props.shipTiles)
-  } 
+    }
 
-  return className;
-}
+    else if (this.props.selected === this.props.id) {
+      className += ' selected'
+      //console.log(this.props.shipTiles)
+    }
 
-checkForShipTile = () => {
-  console.log(this.props.allShipTiles)
-  console.log(this.props.currentId)
-
-  if ((this.props.allShipTiles) && (this.props.allShipTiles.indexOf(this.props.currentId)!==(-1)) ){
-   this.setState({isShipTile:true})
- }
- //this.render()
-}
-
-canCellBeClicked = () => {
-  //console.log('here');
-  if(this.props.hits && this.props.hits.includes(this.props.id)){
-    return false;
+    return className;
   }
-  else if (this.props.misses && this.props.misses.includes(this.props.id)){
-    return false;
-  }
-  else if (this.props.label){
-    return false;
-  }
-  else {
-    return true;
-  }
-}
 
-handleClick = () => {
-  this.checkForShipTile()
-  //console.log('yes');
-  let click = this.canCellBeClicked();
-  if(click){
+  checkForShipTile = () => {
+    console.log(this.props.allShipTiles)
+    console.log(this.props.currentId)
+
+    if ((this.props.allShipTiles) && (this.props.allShipTiles.indexOf(this.props.currentId) !== (-1))) {
+      this.setState({ isShipTile: true })
+    }
+    //this.render()
+  }
+
+  canCellBeClicked = () => {
+    //console.log('here');
+    if (this.props.hits && this.props.hits.includes(this.props.id)) {
+      return false;
+    }
+    else if (this.props.misses && this.props.misses.includes(this.props.id)) {
+      return false;
+    }
+    else if (this.props.label) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  handleClick = () => {
+    this.checkForShipTile()
+    //console.log('yes');
+    let click = this.canCellBeClicked();
+    if (click) {
+      //this.checkForShipTile()
+      this.props.handleSelectTarget(this.props.id, this.props.idNumber)
+    }
     //this.checkForShipTile()
-    this.props.handleSelectTarget(this.props.id, this.props.idNumber)
   }
-  //this.checkForShipTile()
-}
 
-  render () {
+  render() {
     //console.log(this.props.id)
     return (
       //Adding in an id of label for the cells that are not part of the 
       //gameplay but are the labels for the board.
       <div id=
-      {this.props.label 
-        ? 'label' 
-        : ''} 
+        {this.props.label
+          ? 'label'
+          : ''}
         //style={this.state.shipTile ? { 'backgroundColor': 'blue' } : null}
         className={this.determineClassName()}
         onClick={() => this.handleClick()}
-                            >
+      >
         {this.props.id}
       </div>
     )
