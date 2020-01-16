f4232878c1bd3bd1dc7b8773b27f9e8ff1e2974f
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-01-16 10:19:31 -0700

    delete unused function

--- src/Services/game-moves-api-service.js
+++ src/Services/game-moves-api-service.js
@@ -22,30 +22,6 @@ const gameMovesApiService = {
           : res.json()
       )
   },
-
-  //this function accepts the target, gameId, and playerNum(ie: player1 or player2) in order
-  // to check the opponents' ships to see if a hit was made. The return response from the server 
-  //will be a result object containing 'hit' or 'miss' as the result and a ship name or null for ship value.
-  // ie:  result = {result:'hit', ship:'aircraft carrier'} or result = {result:'miss', ship:null}.
-  fireAtTarget(target, gameId, playerNum){
-    return fetch(`${config.API_ENDPOINT}/api/ships/opponent`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        'authorization': `bearer ${TokenService.getAuthToken()}`
-      },
-      body: JSON.stringify({
-        target,
-        gameId,
-        playerNum
-      })
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
-  }
 };
 
 export default gameMovesApiService;
