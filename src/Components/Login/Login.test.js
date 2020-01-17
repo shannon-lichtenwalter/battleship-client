1985128e01b95a26f2f2ba53280bc92cce7c0737
Heesu Kang <hskang135@gmail.com>
2020-01-17 10:35:02 -0500

    minor css change and smoke test for login and signup pass

---
+++ src/Components/Login/Login.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Login';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('Login Form Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render
+      (<BrowserRouter> 
+          <Login /> 
+      </BrowserRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+});
+
+
---
+++ src/Components/Signup/Signup.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Signup from './Signup';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('Login Form Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render
+      (<BrowserRouter> 
+          <Signup /> 
+      </BrowserRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+});
+
+
