69f5a8f94daf6d25fb95785fc1cbf069119dc6eb
Just-A-Fool <aedanscarn@gmail.com>
2020-01-20 15:13:30 -0700

    again just merging with everyone

--- src/Components/UserGrid/UserGrid.js
+++ src/Components/UserGrid/UserGrid.js
@@ -528,22 +528,25 @@ class UserGrid extends React.Component {
         Function called from render to display the UserGrid, the component which the user's ships
         are placed upon
     */
+    //handleRenderGrid instantiates the rows and columns of the gameboard grid with the x and y variables.
+    //it maps over the letters (x) and for each letter returns a 'column' div. When mapping we account for the top 
+    //row of the grid being the letters and the far left column being the number labels. 
     handleRenderGrid = () => {
-        //setting the rows and columns of the gameboard grid
         let y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let x = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-        //map over the letters and for each letter return a 'column' div.
-        //each 'column' div will have a style of 'display:inline-block' so that the
-        // columns will align on the grid.
         return y.map((num, index) => {
             return (
                 <div key={index} className='column'>
-                    {/* These cells will be the top row of the grid and will have a letter for each cell*/}
-                    <Cell id={num} label={true} />
+                    <Cell
+                        id={num}
+                        label={true}
+                    />
                     {x.map((letter, index) => {
                         if (num === 0) {
-                            // these cells will be the most left coulumn and will have the numbers listed in each cell.
-                            return <Cell key={letter} id={letter} label={true} />
+                            return <Cell
+                                key={letter}
+                                id={letter}
+                                label={true} />
                         }
                         return <Cell
                             key={letter + num}
@@ -553,9 +556,7 @@ class UserGrid extends React.Component {
                             shipTiles={this.state.shipOccupied}
                             allShipTiles={this.state.allShipTilesOccupied}
                             shipTileValues={this.state.shipTileValues}
-                            opponentShots={this.state.opponentShots}
-
-                        />
+                            opponentShots={this.state.opponentShots} />
                     })
                     }
                 </div>
