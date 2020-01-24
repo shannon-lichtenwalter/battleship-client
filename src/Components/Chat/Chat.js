a48587fbb4e42f1355f4fb05ad17e579ce7ff723
Just-A-Fool <aedanscarn@gmail.com>
2020-01-24 13:54:12 -0700

    chat changes to add button

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -8,28 +8,28 @@ export default class Chat extends React.Component {
     }
 
     componentDidMount = () => {
-        if(this.props.socket){
-        this.props.socket.on('chat-message', data => {
-            this.setState({
-                messages: [...this.state.messages, {username: data.username, message: data.message}]
+        if (this.props.socket) {
+            this.props.socket.on('chat-message', data => {
+                this.setState({
+                    messages: [...this.state.messages, { username: data.username, message: data.message }]
+                })
+
             })
-            
-        })
-    }
+        }
     }
 
     handleChatMessage = (message) => {
-        
-        this.props.socket.emit('send-message', {room: this.props.room, message: message});
+
+        this.props.socket.emit('send-message', { room: this.props.room, message: message });
         this.setState({
-            messages: [...this.state.messages, {username: 'Me', message}]
+            messages: [...this.state.messages, { username: 'Me', message }]
         })
     }
 
     render() {
 
         let chatHistory = this.state.messages.map((message, index) => {
-            return(
+            return (
                 <li className='chat-li' key={index}>
                     <h4 className={message.username === 'Me' ? 'chat-username chat-me' : 'chat-username chat-opponent'}>{message.username}</h4>
                     <p className='chat-message' aria-live='polite'>{message.message}</p>
@@ -46,10 +46,15 @@ export default class Chat extends React.Component {
                     event.preventDefault();
                     this.handleChatMessage(event.target.chatInput.value);
                     event.target.chatInput.value = '';
-                }}> <fieldset>
+                }}>
+                    <fieldset>
                         {/* <legend>Chat with Opponent</legend> */}
-                        <label htmlFor='chatInput' id='chat-label'>Chat</label>
-                        <input type='text' placeholder='Type Here' id='chatInput' aria-label='type message to opponent here' />
+                        <div id='chat-input-box'>
+                            <label htmlFor='chatInput' id='chat-label'>Chat</label>
+                            <input type='text' placeholder='Type Here' id='chatInput' aria-label='type message to opponent here' />
+                        </div>
+
+                        <button type='submit' id='chat-button'>Submit</button>
                     </fieldset>
                 </form>
             </div>
