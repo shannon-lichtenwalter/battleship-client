f1c8d28ebfc0379bb9fccbd1630a7a8358b7c48f
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-01-24 10:00:44 -0700

    fix button

--- src/Components/OpponentGrid/OpponentGrid.js
+++ src/Components/OpponentGrid/OpponentGrid.js
@@ -245,7 +245,7 @@ class OpponentGrid extends React.Component {
             </select>
           </div>
           <h4 className='selectedTarget'>You have selected target: {this.state.selected}</h4>
-            <Button type='submit' className='fireButton'>Fire!</Button>
+            <button type='submit' className='fireButton'>Fire!</button>
         </fieldset>
       </form>
     )
